8cf5ef28ef82b91b2f0db577737b6057e21393fc
Mateusz Mierzyński <kontakt.mmierzynski@gmail.com>
2021-07-07 19:21:20 +0200

    Created Category and Product entity

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $category = new Category();
+        $category->setName('Category 1')
+            ->setSlug('category-1')
+            ->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+
+        $manager->persist($category);
+
+        for ($i = 1; $i <= 10; $i++){
+            $product = new Product();
+            $product->setName('Product '.$i)
+                ->setPrice((float)mt_rand(10,200))
+                ->setCategory($category)
+                ->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+
+            $manager->persist($product);
+        }
+
+        $manager->flush();
+    }
+}
